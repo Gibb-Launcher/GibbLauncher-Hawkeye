3a48dbffb4f9e54109663d95b330f50011cb8c67
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2018-10-18 22:16:36 -0300

    Change parameters

--- calibration.py
+++ calibration.py
@@ -1,29 +1,39 @@
 import cv2
 import numpy as np
+import imutils
+
  
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('Videos/mesa_ping.mp4')
+cap = cv2.VideoCapture('Videos/teste_mesa_longo.mp4')
+#cv2.imread
 
+count = 0
  
 while True:
     _, frame = cap.read()
  
-    cv2.circle(frame, (155, 120), 5, (0, 0, 255), -1) #1 superior esquerdo
-    cv2.circle(frame, (450, 120), 5, (0, 0, 255), -1) #2 superior direito
-    cv2.circle(frame, (60, 300), 5, (0, 0, 255), -1) #3 inferior esquerdo
-    cv2.circle(frame, (609, 332), 5, (0, 0, 255), -1) #4 inferior direito
+    cv2.circle(frame, (550, 450), 5, (0, 0, 255), -1) #1 superior esquerdo
+    cv2.circle(frame, (1400, 450), 5, (0, 0, 255), -1) #2 superior direito
+    cv2.circle(frame, (150, 1000), 5, (0, 0, 255), -1) #3 inferior esquerdo
+    cv2.circle(frame, (1700, 1000), 5, (0, 0, 255), -1) #4 inferior direito
  
-    pts1 = np.float32([[155, 120], [480, 120], [20, 475], [620, 475]])
-    pts2 = np.float32([[0, 0], [500, 0], [0, 600], [500, 600]])
+    pts1 = np.float32([[550, 450], [1400, 450], [150, 1000], [1700, 1000]])
+#    pts2 = np.float32([[0, 0], [500, 0], [0, 600], [500, 600]])
+    pts2 = np.float32([[0, 0], [600, 0], [0, 700], [600, 700]])
+
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+    result = cv2.warpPerspective(frame, matrix, (750,750))
  
-    result = cv2.warpPerspective(frame, matrix, (500,500))
- 
- 
+    frame = imutils.resize(frame, width=400)
+
     cv2.imshow("Frame", frame)
     
-    
+
     cv2.imshow("Perspective transformation", result)
+    cv2.imwrite("frame%d.jpg" % count,result)
+    count +=1
+
  
     key = cv2.waitKey(1)
     if key == 27:
